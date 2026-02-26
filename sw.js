22d91aa6477c83d23e62c41841ae699d196cd830
vloitz <italocajaleon@gmail.com>
2026-02-26 01:53:08 -0500

    Auto-deploy: Actualizacion desde script

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vloitz-app-v10.6';
+const CACHE_NAME = 'vloitz-app-v10.7';
 const PRELOAD_CACHE_NAME = 'vloitz-tracklist-cache'; // Bóveda de 2s para Latencia Cero
 const ASSETS_TO_CACHE = [
     './',
