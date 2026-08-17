e38bc806ec82cd15473bb052458329f4cff4d765
vloitz <italocajaleon@gmail.com>
2026-08-16 23:40:41 -0500

    Auto-deploy: Actualizacion desde script

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vloitz-app-v41.2';
+const CACHE_NAME = 'vloitz-app-v41.3';
 const PRELOAD_CACHE_NAME = 'vloitz-tracklist-cache'; // Bóveda de 2s para Latencia Cero
 const ASSETS_TO_CACHE = [
     '/',
@@ -221,10 +221,9 @@ self.addEventListener('message', (event) => {
 });
 // --- FIN: RECEPTOR DE CONFIGURACIÓN Y MENSAJES ---
 
-// 1. INSTALACIÓN: Guardamos la interfaz en el caché
+// 1. INSTALACIÓN: Guardamos la interfaz en el caché (Esperando decisión del usuario)
 self.addEventListener('install', (e) => {
-    self.skipWaiting(); // 👊 Activación inmediata sin esperas
-    console.log('[Service Worker] Instalando caché de interfaz...');
+    console.log('[Service Worker] Nueva versión instalada. Esperando la orden del usuario para activarse...');
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
